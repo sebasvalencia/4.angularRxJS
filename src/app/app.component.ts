8aed4b01a1bfabe7b67ceae08e1b05b24e6c47a6
Sebastian Valencia <sebastian.valencia@yuxiglobal.com>
2018-11-28 19:11:28 -0500

    Create first http request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularRxJS';
+  url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+  constructor(private http: HttpClient) {
+    const observable = this.http.get(this.url);
+    observable.subscribe(console.log);
+  }
+
+
 }
