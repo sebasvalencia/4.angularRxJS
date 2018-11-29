3c8dcf291b552b2966bd0faece04e7eac0c0f7d9
Sebastian Valencia <sebastian.valencia@yuxiglobal.com>
2018-11-28 19:38:19 -0500

    Create Todo interface use in the ID

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Todo } from './todo.interface';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +11,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angularRxJS';
   url = 'https://jsonplaceholder.typicode.com/todos/1';
+  urlAssets = '/src/assets/todo.json';
+  todo: string;
 
   constructor(private http: HttpClient) {
+    // const observable = this.http.get(this.urlAssets);
     const observable = this.http.get(this.url);
-    observable.subscribe(console.log);
+    observable.subscribe(
+      (todo: Todo) => this.todo = todo.title
+      );
   }
 
 
